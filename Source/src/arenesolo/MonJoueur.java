0a4650143ca53ecd6c9309d13d864e922f46ead1
Chiappero <mot2passe>
2018-03-19 13:33:16 +0100

    commentaire

--- Source/src/arenesolo/MonJoueur.java
+++ Source/src/arenesolo/MonJoueur.java
@@ -24,57 +24,47 @@ public class MonJoueur extends jeu.Joueur {
      * @param y0 position Y initiale
      * @param distanceMax distance a parcourir
      */
-    void move(int x0, int y0, int distanceMax) {
+    void move(Plateau etatdujeu, Point p, int distanceMax) {
+        int x0=p.x;
+        int y0=p.y;
+        System.out.println("current position = "+String.valueOf(x0)+" "+String.valueOf(y0));
         // directions possibles: G=(-1,0) H=(0,-1) D=(1,0) B=(0,1)
         int[] dx = new int[] {-1,0,1,0};
         int[] dy = new int[] {0,-1,0,1};
         int dirIndex=0;
-
         // distance parcourue
         int distance=0;
-
         // nombre de pas a faire
         int stepToDo=1;
-
         // position courante
         int x=x0, y=y0;
         //System.out.println("Initial position: " +x+","+y);
-
         while (true) {
-
             // a faire 2 fois avec le meme nombre de pas (gauche+haut) ou (droite+bas)
             for (int i = 0; i < 2; i++) {
-
                 // déplacement du nombre de pas
                 for (int j = 0; j < stepToDo; j++) {
-
                     // condition de sortie
                     distance++;
                     if(distance>distanceMax) return;
-
                     // déplacement
                     x+=dx[dirIndex];
                     y+=dy[dirIndex];
-
-                    System.out.println("Current position: " +x+","+y+" distance="+distance);
+                    System.out.println("Current position: " +x+","+y+" distance="+distance+" contenu = "+etatdujeu.donneContenuCellule(x,y));
                 }
-
                 // tourne a droite
                 dirIndex = (dirIndex + 1) % 4;
             }
-
             // incrementer le nombre de pas a faire
             stepToDo++;
         }
-
     }
 
     // action
     @Override
     public Action faitUneAction(Plateau etatDuJeu) {
-        chercheProche(etatDuJeu, this.donnePosition());
-        return Action;
-        etatDuJeu.
+        move(etatDuJeu, this.donnePosition(),16);
+        return Action.RIEN;
         //return super.faitUneAction(etatDuJeu);
 
     }
@@ -84,8 +74,11 @@ public class MonJoueur extends jeu.Joueur {
         System.out.println("La partie est finie.");
         // destructions des variables, nettoyage, enregistrement des logs et statistiques, etc...
     }
+    /*
+    public void chercheProche(Plateau etatDuJeu, Point p){
+        p.x=p.x+1;
 
-    public void chercheProche(Plateau etatDuJeu, Point position){
-
+        etatDuJeu.donneContenuCellule(p);
     }
+    */
 }
