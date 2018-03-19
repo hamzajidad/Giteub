aa7d22fed65cee7d3e2d3d1853f6c767acb10e7a
germain96 <g.ratsimarisolo@gmail.com>
2018-03-19 12:20:44 +0100

    testGermain

--- Source/src/arenesolo/MonJoueur.java
+++ Source/src/arenesolo/MonJoueur.java
@@ -81,6 +81,7 @@ public class MonJoueur extends jeu.Joueur {
     //commentaries
     @Override
     protected void finDePartie(String lePlateau) {
+        //testGermain
         System.out.println("La partie est finie.");
         // destructions des variables, nettoyage, enregistrement des logs et statistiques, etc...
     }
