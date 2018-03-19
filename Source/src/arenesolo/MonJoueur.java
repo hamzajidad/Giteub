e7bec1129bbb325fcd8a9200022d52dc62a55832
jeremywasner <wasner83@gmail.com>
2018-03-19 11:59:13 +0100

    Ajout Fonction déplacement spirale

--- Source/src/arenesolo/MonJoueur.java
+++ Source/src/arenesolo/MonJoueur.java
@@ -17,11 +17,64 @@ public class MonJoueur extends jeu.Joueur {
         // initialisation des algorithms, etc...
     }
 
+    /**
+     * Parcours en spirale
+     *
+     * @param x0 position X initiale
+     * @param y0 position Y initiale
+     * @param distanceMax distance a parcourir
+     */
+    void move(int x0, int y0, int distanceMax) {
+        // directions possibles: G=(-1,0) H=(0,-1) D=(1,0) B=(0,1)
+        int[] dx = new int[] {-1,0,1,0};
+        int[] dy = new int[] {0,-1,0,1};
+        int dirIndex=0;
+
+        // distance parcourue
+        int distance=0;
+
+        // nombre de pas a faire
+        int stepToDo=1;
+
+        // position courante
+        int x=x0, y=y0;
+        //System.out.println("Initial position: " +x+","+y);
+
+        while (true) {
+
+            // a faire 2 fois avec le meme nombre de pas (gauche+haut) ou (droite+bas)
+            for (int i = 0; i < 2; i++) {
+
+                // déplacement du nombre de pas
+                for (int j = 0; j < stepToDo; j++) {
+
+                    // condition de sortie
+                    distance++;
+                    if(distance>distanceMax) return;
+
+                    // déplacement
+                    x+=dx[dirIndex];
+                    y+=dy[dirIndex];
+
+                    System.out.println("Current position: " +x+","+y+" distance="+distance);
+                }
+
+                // tourne a droite
+                dirIndex = (dirIndex + 1) % 4;
+            }
+
+            // incrementer le nombre de pas a faire
+            stepToDo++;
+        }
+
+    }
+
     // action
     @Override
     public Action faitUneAction(Plateau etatDuJeu) {
         chercheProche(etatDuJeu, this.donnePosition());
         return Action;
+        etatDuJeu.
         //return super.faitUneAction(etatDuJeu);
 
     }
