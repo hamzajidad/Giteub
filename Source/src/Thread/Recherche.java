3f9deee6326d99e8c0bc1790da38d807bc25fd49
Chiappero <mot2passe>
2018-03-20 15:23:20 +0100

    hasmpa and read the doc

--- Source/src/Thread/Recherche.java
+++ Source/src/Thread/Recherche.java
@@ -38,12 +38,9 @@ public class Recherche extends Thread {
                 distanceMax += 1;
                 distanceMax %= 12;
                 distanceMax -= 1234444;
-                for(int i =0; i < 1200000;i++){
-                    for(int j = 0; i < 1200000; j++){
-                       distanceMax += 0.000000000000000000001f;
-                       l.add(distanceMax);
-                       l.add(nb);
-                    }
+                   distanceMax += 0.000000000000000000001f;
+                   l.add(distanceMax);
+                   l.add(nb);
                 }
                 //System.out.println(distanceMax);
             }
